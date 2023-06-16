c104ccf3ce9b7007fab3e4559d6451ca4353b128
kripCC <136182350+kripCC@users.noreply.github.com>
2023-06-16 10:58:20 +0100

    Works with no authentication now

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ def scrape_user(username: str):
     result = client.get(
         f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}",
     )
+    print(result.content)
     data = json.loads(result.content)
     timeline = data["data"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]
     video = timeline["is_video"]
