f4439876e17f7b2aeb97cf0dc85422ba8d1e32fc
kripCC <136182350+kripCC@users.noreply.github.com>
2023-06-19 20:52:00 +0100

    Added proxy support

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from apify import Actor
-from instagrapi import Client
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pprint import pprint
+from selenium_stealth import stealth
 import json
 import requests
 from urllib.parse import quote
@@ -9,41 +12,65 @@ headers={
         # use browser-like features
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "Accept": "*/*",
     }
 
+def prepare_browser(proxy_url):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument(f'--proxy-server={proxy_url}')
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(options= chrome_options)
+    stealth(driver,
+            user_agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+            languages= ["en-US", "en"],
+            vendor=  "Google Inc.",
+            platform=  "Win32",
+            webgl_vendor=  "Intel Inc.",
+            renderer=  "Intel Iris OpenGL Engine",
+            fix_hairline= False,
+            run_on_insecure_origins= False,
+            )
+    return driver
 
 async def main():
     async with Actor:
-        proxy_configuration = await Actor.create_proxy_configuration()
-        proxy_url = await proxy_configuration.new_url()
-        proxies = {
-         'http': proxy_url,
-         'https': proxy_url,
-        }
         actor_input = await Actor.get_input() or {}
+        proxy_settings = actor_input.get('proxySettings')
+        proxy_configuration = await Actor.create_proxy_configuration(actor_proxy_input=proxy_settings)
+        proxy_url = await proxy_configuration.new_url()
         usernames = actor_input.get("usernames")
         for username in usernames:
             print(username)
-            result = requests.get(f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}", headers=headers, proxies=proxies)
-            print(result.text)
-            data = json.loads(result.text)
-            timeline = data["data"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]
-            video = timeline["is_video"]
-            caption = timeline["edge_media_to_caption"]["edges"][0]["node"]["text"]
-            thumbnail = timeline["thumbnail_src"]
-            url = ""
-            if(video==True):
-                url = timeline["video_url"]
+            url = f'https://instagram.com/{username}/?__a=1&__d=dis'
+            chrome = prepare_browser(proxy_url)
+            chrome.get(url)
+            print(f"Attempting: {chrome.current_url}")
+            if "login" in chrome.current_url:
+                print("Failed/ redir to login")
+                chrome.quit()
             else:
-                url = timeline["display_url"]
+                print ("Success")
+                resp_body = chrome.find_element(By.TAG_NAME, "body").text
+                data_json = json.loads(resp_body)
+                data = data_json['graphql']['user']
+                timeline = data["data"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]
+                video = timeline["is_video"]
+                caption = timeline["edge_media_to_caption"]["edges"][0]["node"]["text"]
+                thumbnail = timeline["thumbnail_src"]
+                url_ig = ""
+                if(video==True):
+                    url_ig = timeline["video_url"]
+                else:
+                    url_ig = timeline["display_url"]
             await Actor.push_data({
                         'username': username,
                         'video': video,
                         'caption': caption,
                         'thumbnail': thumbnail,
-                        'url': url
+                        'url': url_ig
                         })
                     
 
