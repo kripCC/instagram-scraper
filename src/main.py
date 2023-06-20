2fc9654d18a4e70d5a306fdf95b6eb87321a972e
kripCC <136182350+kripCC@users.noreply.github.com>
2023-06-20 01:19:25 +0100

    Added proxy support

--- src/main.py
+++ src/main.py
@@ -12,13 +12,18 @@ from urllib.parse import quote
 async def main():
     async with Actor:
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument(f'--proxy-server={proxy_url}')
         chrome_options.add_argument("start-maximized")
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         chrome_options.add_experimental_option('useAutomationExtension', False)
-        driver = webdriver.Chrome(options= chrome_options)
+
+        actor_input = await Actor.get_input() or {}
+        proxy_settings = actor_input.get('proxySettings')
+        proxy_configuration = await Actor.create_proxy_configuration(actor_proxy_input=proxy_settings)
+        proxy_url = await proxy_configuration.new_url()
+        usernames = actor_input.get("usernames")
+        chrome_options.add_argument(f'--proxy-server={proxy_url}')
         stealth(driver,
                 user_agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
                 languages= ["en-US", "en"],
@@ -29,24 +34,18 @@ async def main():
                 fix_hairline= False,
                 run_on_insecure_origins= False,
                 )
-        actor_input = await Actor.get_input() or {}
-        proxy_settings = actor_input.get('proxySettings')
-        proxy_configuration = await Actor.create_proxy_configuration(actor_proxy_input=proxy_settings)
-        proxy_url = await proxy_configuration.new_url()
-        print(proxy_url)
-        usernames = actor_input.get("usernames")
+        driver = webdriver.Chrome(options= chrome_options)
         for username in usernames:
             print(username)
             url = f'https://instagram.com/{username}/?__a=1&__d=dis'
-            chrome = prepare_browser(proxy_url)
-            chrome.get(url)
-            print(f"Attempting: {chrome.current_url}")
-            if "login" in chrome.current_url:
+            driver.get(url)
+            print(f"Attempting: {driver.current_url}")
+            if "login" in driver.current_url:
                 print("Failed/ redir to login")
-                chrome.quit()
+                driver.quit()
             else:
                 print ("Success")
-                resp_body = chrome.find_element(By.TAG_NAME, "body").text
+                resp_body = driver.find_element(By.TAG_NAME, "body").text
                 data_json = json.loads(resp_body)
                 data = data_json['graphql']['user']
                 timeline = data["data"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]
